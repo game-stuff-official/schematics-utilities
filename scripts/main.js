2e4f890fd35a72229156572e8593ee836bc4a6bf
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-03-07 08:20:27 -0600

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const overflowGate = extend(Block, "titanium-overflow-gate", {
+  name: 'Titanium Overflow Gate',
+  research: 'overflow-gate',
+  buildCostMultiplier: 3,
+  requierments: [
+    lead/4,
+    copper/6,
+    titanium/3
+  ]
+});
+
+
+/*name: Titanium Overflow Gate
+research: overflow-gate
+buildCostMultipier: 3
+requirements: [
+  lead/4
+  copper/6
+  titanium/3
+]*/
