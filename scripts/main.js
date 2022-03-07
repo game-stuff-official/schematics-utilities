c7e5a64f3aedb6f281a8c6d78ca5213e6373b0b5
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-03-07 08:29:56 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,4 @@
-const overflowGate = extend(Block, "titanium-overflow-gate", {
-  name: 'Titanium Overflow Gate',
-  research: 'overflow-gate',
-  buildCostMultiplier: 3,
-  maxConsecutive: 4,
-  requierments: [
-    'lead'/4,
-    'copper'/6,
-    'titanium'/3
-  ]
-});
+titanium-overflow-gate.maxConsecutive = 4
 
 
 /*name: Titanium Overflow Gate
